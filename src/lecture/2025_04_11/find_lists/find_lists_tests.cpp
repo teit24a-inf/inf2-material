d05517c98a22c3c60f70f8659aa040352cd20503
Reiner Huechting <dev@reinerhuechting.de>
2025-04-11 15:12:48 +0200

    Fehlgeschlagenen Test entfernt.

--- src/lecture/2025_04_11/find_lists/find_lists_tests.cpp
+++ src/lecture/2025_04_11/find_lists/find_lists_tests.cpp
@@ -5,5 +5,4 @@
 TEST_CASE("find_element", "[find]") {
   REQUIRE(find_element(std::vector{10, 20, 30, 40, 50}, 30) == 2);
   REQUIRE(find_element(std::vector{10, 20, 30, 40, 50}, 100) == 5);
-  REQUIRE(find_element(std::vector{10, 20, 30, 40, 50}, 100) == 6);
 }
