24091088aabdd8b0c9d90530fe4e99028ccfad85
Reiner Huechting <dev@reinerhuechting.de>
2025-04-11 13:14:07 +0200

    Zwischenstand aus Vorlesung.

--- src/lecture/2025_04_11/vectors2.cpp
+++ src/lecture/2025_04_11/vectors2.cpp
@@ -1,9 +1,38 @@
 #include "vectors2.h"
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
-int main() { list_iteration(); }
+int main() {
+  // list_iteration();
+  list_find();
+}
+
+void list_find() {
+  std::vector<int> v2{10, 17, 55, 23, 42, 55, 107, 38};
+
+  // Herausfinden, an welcher Stelle die 55 steht.
+  int result = 0;
+  for (int i = 0; i < v2.size(); i++) {
+    if (v2[i] == 55) {
+      result = i;
+      break;
+    }
+  }
+  std::cout << result << std::endl;
+
+  // Bessere Methode:
+  auto pos_55 = std::find(v2.begin(), v2.end(), 55);
+  if (pos_55 != v2.end()) {
+    std::cout << pos_55 - v2.begin() << std::endl;  // Stelle ausgeben.
+    std::cout
+        << *pos_55
+        << std::endl;  // Durch den Iterator wieder auf den Wert zugreifen.
+  } else {
+    std::cout << "Wert nicht gefunden!\n";
+  }
+}
 
 void list_iteration() {
   std::vector<int> v1;
--- src/lecture/2025_04_11/vectors2.h
+++ src/lecture/2025_04_11/vectors2.h
@@ -4,4 +4,7 @@
 /// Beispiele für den Aufbau und das Iterieren über Vektoren.
 void list_iteration();
 
+/// Beispiele für die Lineare Suche in Listen.
+void list_find();
+
 #endif
